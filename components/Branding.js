690f44dbed27a424c565d3f5be85ba447adb950a
Peter Esezobor <70745960+peteCoder@users.noreply.github.com>
2022-03-18 20:44:39 +0100

    committed

--- components/Branding.js
+++ components/Branding.js
@@ -5,39 +5,42 @@ const Branding = () => {
   return (
     <section className="flex flex-col md:flex-row 
         justify-center items-center mt-10 gap-6 px-8 max-w-[1400px] mx-auto">
+            
             <div className="brand group">
                 <Image src="/images/disney_.png" layout="fill" objectFit="cover" />
                 <video autoPlay loop playsInline className="hidden group-hover:inline
-                    rounded-lg object-cover w-[100%]">
-                    <source src="/videos/disney.mp4" />
+                    rounded-lg object-cover ">
+                    <source src="/videos/disney.mp4" type="video/mp4" />
                 </video>
             </div>
+
             <div className="brand group">
                 <Image src="/images/marvel.png" layout="fill" objectFit="cover" />
                 <video autoPlay loop playsInline className="hidden group-hover:inline
                     rounded-lg object-cover w-[100%]">
-                    <source src="/videos/marvel.mp4" />
+                    <source src="/videos/marvel.mp4" type="video/mp4" />
                 </video>
             </div>
+
             <div className="brand group">
                 <Image src="/images/pixar.png" layout="fill" objectFit="cover" />
                 <video autoPlay loop playsInline className="hidden group-hover:inline
                     rounded-lg object-cover w-[100%]">
-                    <source src="/videos/pixar.mp4" />
+                    <source src="/videos/pixar.mp4" type="video/mp4" />
                 </video>
             </div>
             <div className="brand group">
                 <Image src="/images/starwars.png" layout="fill" objectFit="cover" />
                 <video autoPlay loop playsInline className="hidden group-hover:inline
                     rounded-lg object-cover w-[100%]">
-                    <source src="/videos/star-wars.mp4" />
+                    <source src="/videos/star-wars.mp4" type="video/mp4" />
                 </video>
             </div>
             <div className="brand group">
                 <Image src="/images/national-geographic.png" layout="fill" objectFit="cover" />
                 <video autoPlay loop playsInline className="hidden group-hover:inline
                     rounded-lg object-cover w-[100%]">
-                    <source src="/videos/national-geographic.mp4" />
+                    <source src="/videos/national-geographic.mp4" type="video/mp4" />
                 </video>
             </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "home": "url('/images/background.png')"
+      })
+    },
     fontFamily: {
       body: ["Montserrat", "sans-serif"]
     }
